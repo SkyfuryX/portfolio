bffcee86c584825be96124511873521ebd42291c
SkyfuryX <gannonshanley@gmail.com>
2026-02-16 18:08:09 -0500

    models/model admin adjusted

--- projects/models.py
+++ projects/models.py
@@ -3,13 +3,21 @@ from datetime import datetime
 from django.utils.text import slugify
 
 class Project(models.Model):
-    cover_image = models.ImageField(upload_to="images", null=True)
+    cover_image = models.ImageField(upload_to="images", null=True, blank=True)
     title = models.CharField(max_length=100)
+    LangChoices = {
+        "PY" : "Python",
+        "GO" : "Golang",
+        "SQ" : "SQL",
+        "OT" : "Other"
+    }
+    language = models.CharField(choices=LangChoices, max_length=2, default = "PY")
     tech = models.CharField()
-    description = models.CharField()
+    description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
-    slug = models.SlugField(default=slugify(title))
+    slug = models.SlugField(default="", null=False)
     
     def __str__(self):
         return self.title
+    
