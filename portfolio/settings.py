abfd04948a7cadfa4571ffca73e57d9bbf6f85e6
SkyfuryX <gannonshanley@gmail.com>
2026-07-16 10:30:17 -0400

    updated settings file for local development. Updated SVG icon on base template.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 if DEBUG:
     load_dotenv(override=True) #For local development
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SSL Configuration for AWS Reverse Proxy
 SECURE_PROXY_SSL_HEADER = ('HTTP_CLOUDFRONT_FORWARDED_PROTO', 'https')
